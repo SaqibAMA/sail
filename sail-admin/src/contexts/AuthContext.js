5b8140d227080f6360246ab02394279c544a6bd4
Saqib Ali <business.saqib@gmail.com>
2022-04-11 23:57:47 +0500

    Chore: Isolated Admin Verification in DAL

--- sail-admin/src/contexts/AuthContext.js
+++ sail-admin/src/contexts/AuthContext.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState, createContext } from "react";
-import { app, db } from "../firebase";
+import { app } from "../firebase";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { collection, getDocs } from "firebase/firestore";
+
+import { verifyAdmin } from "../DAL/auth";
 
 const AuthContext = createContext();
 
@@ -17,31 +18,22 @@ export const AuthProvider = ({ children }) => {
   const provider = new GoogleAuthProvider();
 
   const signInWithGoogle = () => {
+    setLoginError(false);
     signInWithPopup(auth, provider)
       .then((res) => {
-
-        // access the allowed admins db
-        getDocs(collection(db, "admin-accounts")).then((querySnapshot) => {
-          
-          // by default assume that user is not allowed
-          setCurrentUser(null);
-          
-          // check if there are any admins that match the signed-in email
-          querySnapshot.forEach(doc => {
-            // if admin email is found, set currentUser to the admin
-            if (doc.data().email === res.user.email) {
-                setCurrentUser({
-                  email: res.user.email,
-                  profileImage: res.user.photoURL,
-                });
-            }
-          });
-
-          if (!currentUser) {
+        setCurrentUser(null);
+        verifyAdmin(
+          res.user.email,
+          () => {
+            setCurrentUser({
+              email: res.user.email,
+              profileImage: res.user.photoURL,
+            });
+          },
+          () => {
             setLoginError(true);
           }
-
-        });
+        );
       })
       .catch((err) => {
         setCurrentUser(null);
