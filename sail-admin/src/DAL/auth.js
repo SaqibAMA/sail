bf3d8d9ae2f33ab220b9ffadf6b0ea8c2d87da5d
Saqib Ali <business.saqib@gmail.com>
2022-04-12 00:03:23 +0500

    Fix: Updated onFailure() in verifyAdmin()

--- sail-admin/src/DAL/auth.js
+++ sail-admin/src/DAL/auth.js
@@ -2,18 +2,16 @@ import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 export const verifyAdmin = (email, onSuccess, onFailure) => {
-
   // access the allowed admins db
   getDocs(collection(db, "admin-accounts")).then((querySnapshot) => {
     // check if there are any admins that match the signed-in email
     querySnapshot.forEach((doc) => {
       // if admin email is found, set currentUser to the admin
       if (doc.data().email === email) {
-          onSuccess();
+        onSuccess();
       }
     });
+
+    onFailure();
   });
-
-  onFailure();
-
 };
