bb242f497a669948a99ad4ee0fb7f8e37c823ac8
Saqib Ali <business.saqib@gmail.com>
2022-05-17 22:38:11 +0500

    Feat: Added Ad-Banner-API Microservice

---
+++ sail-microservices/ad-banner-api/index.js
@@ -0,0 +1,58 @@
+// express for api
+const express = require('express');
+const app = express();
+
+// port
+const PORT = process.env.PORT || 3000;
+
+// import sharp for image processing
+const sharp = require('sharp');
+
+// add firebase
+const {admin, serviceAccount} = require('./firebase.js');
+const store = 'firebase-storage';
+
+// access firebase storage
+const storage = admin.storage();
+
+// bucket link: gs://sail-app-c1585.appspot.com
+const bucket = storage.bucket('gs://sail-app-c1585.appspot.com');
+
+// default route
+app.get('/', (req, res) => {
+
+    bucket.getFiles()
+    .then((results) => {
+        
+        // if the file is within "banners" folder, then print it's public link
+        const files = results[0].filter(file => file.name.includes('banners'));
+
+        // filter out the files that are not in the folder "banners"
+        const filteredFiles = files.filter(file => file.name.includes('banners'));
+
+        // iterate over filteredFiles, generate their signedURL and then send them to the client
+        let urls = [];
+        filteredFiles.forEach(file => {
+            
+            urls.push(
+
+                file.getSignedUrl({
+                    action: 'read',
+                    expires: '03-09-2491'
+                })
+
+            );
+
+        });
+
+        Promise.all(urls).then(allURLs => {
+            res.send(allURLs.map(url => url[0]));
+        });
+
+    });
+
+});
+
+
+// listen
+app.listen(PORT, () => console.log("Server started on port " + PORT));
